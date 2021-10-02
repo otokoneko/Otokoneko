227ea167c434bd1e9cb45544c027034114d7e630
otokoneko <1506966990@qq.com>
2021-10-02 10:22:35 +0800

    更新dmzj插件

--- Otokoneko.Plugins/Otokoneko.Plugins.Dmzj/DmzjDownloader.cs
+++ Otokoneko.Plugins/Otokoneko.Plugins.Dmzj/DmzjDownloader.cs
@@ -11,7 +11,7 @@ using System.Threading.Tasks;
 using HtmlAgilityPack;
 using Newtonsoft.Json;
 using Dmzj;
-
+using System.Net;
 
 namespace Otokoneko.Plugins.Dmzj
 {
@@ -47,35 +47,46 @@ namespace Otokoneko.Plugins.Dmzj
     {
         public string Name => nameof(Dmzj);
         public string Author => "Otokoneko";
-        public Version Version => new Version(1, 0, 0);
+        public Version Version => new Version(1, 0, 1);
         private static string MangaApiBase { get; } = "https://nnv4api.muwai.com/comic/detail/{0}?uid=1";
-        private static string ChapterApiBase { get; } = "https://m.dmzj.com/view/{0}/{1}.html";
+        private static string ChapterApiBase { get; } = "https://m.dmzj.com/chapinfo/{0}/{1}.html";
         private static Regex MangaIdRe { get; } = new Regex("(?:(?:obj_id)|(?:g_current_id)) = \"([0-9]+)\"");
         private static Regex ImageListRe { get; } = new Regex("\"page_url\":(.+?]),");
         private static HttpClient Client { get; } = new HttpClient(new RetryHandler(new HttpClientHandler()));
 
-        private byte[] PrivateKey { get; } = Convert.FromBase64String(
-            "MIICXgIBAAKBgQCvJzUdZU5yHyHrOqEViTY95gejrLAxsdLhjKYKW1QqX+vlcJ7iNrLZoWTaEHDONeyM+1qpT821JrvUeHRCpixhBKjoTnVWnofV5NiDz46iLuU25C2UcZGN3STNYbW8+e3f66HrCS5GV6rLHxuRCWrjXPkXAAU3y2+CIhY0jJU7JwIDAQABAoGBAIs/6YtoSjiSpb3Ey+I6RyRo5/PpS98GV/i3gB5Fw6E4x2uO4NJJ2GELXgm7/mMDHgBrqQVoi8uUcsoVxaBjSm25737TGCueoR/oqsY7Qy540gylp4XAe9PPbDSmhDPSJYpersVjKzDAR/b9jy3WLKjAR6j7rSrv0ooHhj3oge1RAkEA4s1ZTb+u4KPfUACL9p/4GuHtMC4s1bmjQVxPPAHTp2mdCzk3p4lRKrz7YFJOt8245dD/6c0M8o4rcHuh6AgCKQJBAMWzrZwptbihKeR7DWlxCU8BO1kH+z6yw+PgaRrTSpII2un+heJXeEGdk0Oqr7Aos0hia4zqTXY1Rie24GDHHM8CQQC7yVjy5g4u06BXxkwdBLDR2VShOupGf/Ercfns7npHuEueel6Zajn5UAY2549j4oMATf9Gn0/kGVDgTo1s6AyZAkApc6PqA0DLxlbPRhGo0v99pid4YlkGa1rxM4M2Eakn911XBHuz2l0nfM98t5QAnngArEoakKHPMBpWh1yCTh03AkEAmcOddu2RrPGQ00q6IKx+9ysPx71+ecBgHoqymHL9vHmrr3ghu4shUdDxQfz/xA2Z8m/on78hBZbnD1CNPmPOxQ==");
-
+        private const string DefaultKey = "MIICXgIBAAKBgQCvJzUdZU5yHyHrOqEViTY95gejrLAxsdLhjKYKW1QqX+vlcJ7iNrLZoWTaEHDONeyM+1qpT821JrvUeHRCpixhBKjoTnVWnofV5NiDz46iLuU25C2UcZGN3STNYbW8+e3f66HrCS5GV6rLHxuRCWrjXPkXAAU3y2+CIhY0jJU7JwIDAQABAoGBAIs/6YtoSjiSpb3Ey+I6RyRo5/PpS98GV/i3gB5Fw6E4x2uO4NJJ2GELXgm7/mMDHgBrqQVoi8uUcsoVxaBjSm25737TGCueoR/oqsY7Qy540gylp4XAe9PPbDSmhDPSJYpersVjKzDAR/b9jy3WLKjAR6j7rSrv0ooHhj3oge1RAkEA4s1ZTb+u4KPfUACL9p/4GuHtMC4s1bmjQVxPPAHTp2mdCzk3p4lRKrz7YFJOt8245dD/6c0M8o4rcHuh6AgCKQJBAMWzrZwptbihKeR7DWlxCU8BO1kH+z6yw+PgaRrTSpII2un+heJXeEGdk0Oqr7Aos0hia4zqTXY1Rie24GDHHM8CQQC7yVjy5g4u06BXxkwdBLDR2VShOupGf/Ercfns7npHuEueel6Zajn5UAY2549j4oMATf9Gn0/kGVDgTo1s6AyZAkApc6PqA0DLxlbPRhGo0v99pid4YlkGa1rxM4M2Eakn911XBHuz2l0nfM98t5QAnngArEoakKHPMBpWh1yCTh03AkEAmcOddu2RrPGQ00q6IKx+9ysPx71+ecBgHoqymHL9vHmrr3ghu4shUdDxQfz/xA2Z8m/on78hBZbnD1CNPmPOxQ==";
+        
         #region RequiredParameters
 
         [RequiredParameter(typeof(string), @"^https?://manhua.dmzj1?.com/[^/]+/?$", alias: "漫画链接正则表达式")]
         public string MangaRe { get; set; }
 
-        [RequiredParameter(typeof(string), @"^https?://m.dmzj1?.com/view/[0-9]+/[0-9]+\.html/?$", alias: "章节链接正则表达式")]
+        [RequiredParameter(typeof(string), @"^https?://m.dmzj.com/chapinfo/[0-9]+/[0-9]+\.html/?$", alias: "章节链接正则表达式")]
         public string ChapterRe { get; set; }
 
         [RequiredParameter(typeof(string), @"^https?://images.dmzj1?.com/.+$", alias: "图片链接正则表达式")]
         public string ImageRe { get; set; }
 
-        [RequiredParameter(typeof(string), "author", nameof(TagTypeAuthorName), alias:"标签类别“作者”的默认名称")]
+        [RequiredParameter(typeof(string), "author", alias:"标签类别“作者”的默认名称")]
         public string TagTypeAuthorName { get; set; }
 
-        [RequiredParameter(typeof(string), "content", nameof(TagTypeContentName), alias:"标签类别“内容”的默认名称")]
+        [RequiredParameter(typeof(string), "content", alias:"标签类别“内容”的默认名称")]
         public string TagTypeContentName { get; set; }
 
-        [RequiredParameter(typeof(string), "status", nameof(TagTypeStatusName), alias:"标签类别“连载状态”的默认名称")]
+        [RequiredParameter(typeof(string), "status", alias: "标签类别“连载状态”的默认名称")]
         public string TagTypeStatusName { get; set; }
+
+        [RequiredParameter(typeof(string), DefaultKey, alias: "接口RSA私钥")]
+        public string PrivateKey { get; set; }
+
+        #endregion
+
+        #region JsonObject
+
+        public class Chapter
+        {
+            public List<string> page_url { get; set; }
+        }
 
         #endregion
 
@@ -110,11 +121,11 @@ namespace Otokoneko.Plugins.Dmzj
             return decrypted.ToArray();
         }
 
-        private byte[] Decrypt(string data)
+        private byte[] Decrypt(string data, string key)
         {
             var bytes = Convert.FromBase64String(data);
             using var rsa = new RSACryptoServiceProvider();
-            rsa.ImportRSAPrivateKey(PrivateKey, out _);
+            rsa.ImportRSAPrivateKey(Convert.FromBase64String(key), out _);
             return RsaDecrypt(bytes, rsa);
         }
 
@@ -137,7 +148,7 @@ namespace Otokoneko.Plugins.Dmzj
 
             var apiUrl = string.Format(MangaApiBase, mangaId);
             var apiResponse = await Client.GetStringAsync(apiUrl);
-            var mangaDetailResponse = MangaDetailResponse.Parser.ParseFrom(Decrypt(apiResponse));
+            var mangaDetailResponse = MangaDetailResponse.Parser.ParseFrom(Decrypt(apiResponse, PrivateKey));
 
             var mangaDetail = mangaDetailResponse.Manga;
             var result = new MangaDetail()
@@ -170,10 +181,9 @@ namespace Otokoneko.Plugins.Dmzj
 
         public async ValueTask<List<string>> GetChapter(string url)
         {
-            var page = await Client.GetStringAsync(url);
-            var imageListMatch = ImageListRe.Match(page);
-            if (!imageListMatch.Success) return null;
-            var imageList = JsonConvert.DeserializeObject<List<string>>(imageListMatch.Groups[1].Value);
+            var resp = await Client.GetStringAsync(url);
+            var chapter = JsonConvert.DeserializeObject<Chapter>(resp);
+            var imageList = chapter.page_url.Where(url => url != "https://images.dmzj.com/").ToList(); // dmzj 部分漫画的图片下载链接有误，直接过滤
             return imageList;
         }
 
